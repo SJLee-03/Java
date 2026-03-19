c82c0e6d139b2e0a6bfdcefb641e9d5c9d77ed57
SJLEE-03 <sjleego03@gmail.com>
2026-03-19 16:53:19 +0900

    day2

---
+++ src/chap_01/_04_Comment.java
@@ -0,0 +1,8 @@
+package chap_01;
+
+public class _04_Comment {
+    public static void main(String[] args) {
+//        한줄 주석 단축키 : Ctrl + /
+//        여러 줄 주석 단축키 : Ctrl + Shift + /
+    }
+}
---
+++ src/chap_01/_05_Naming.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _05_Naming {
+    public static void main(String[] args) {
+//        변수 이름 짓는 법
+//        1. 저장할 값에 어울리는 이름
+//        2. 밑줄(_), 문자(abc), 숫자(123) 사용 가능 (공백 사용 불가)
+//        3. 밑줄 또는 문자로 시작 가능
+//        4. 한 단어 또는 2개 이상 단어의 연속
+//        5. 변수는 소문자로 시작, 2개 이상의 단어인 경우 각 단어의 시작 글자는 대문자 (첫 문자는 제외) ex) dateOfBirth
+//        6. 예약어 사용 불가 (public, static, void, float, double, int)
+    }
+}
